3f884956a1816dade3656c6c227b8c92301a6984
jesrolcad <jesrolcad@alum.us.es>
2023-07-03 23:01:49 +0200

    GET SONG SERVICE
    Added function to song service
    to get song by id. Modified song
    model mapper to return None in
    case song parameter is None
    
    Roldán Cadena, Jesús <@jesrolcad>

--- app/logic/model_mappers/song_model_mapper.py
+++ app/logic/model_mappers/song_model_mapper.py
@@ -14,6 +14,9 @@ def map_to_song_entity(song: Song)->SongEntity:
 
 
 def map_to_song_model(song_entity: SongEntity)->Song:
+    if song_entity is None:
+        return None
+    
     event = event_model_mapper.map_to_event_model(song_entity.event)
     country = country_model_mapper.map_to_country_model(song_entity.country)
     return Song(id=song_entity.id, title=song_entity.title, artist=song_entity.artist, 
