7972738e893d5a887ccd5ec1a57c9e5fb1481f3a
jesrolcad <jesrolcad@alum.us.es>
2023-07-12 18:20:12 +0200

    ADD 400 RESPONSE IN UPDATE SONG ENDPOINT
    Roldán Cadena, Jesús <@jesrolcad>

--- app/routers/endpoints/definitions/song_definitions.py
+++ app/routers/endpoints/definitions/song_definitions.py
@@ -43,7 +43,13 @@ update_song_endpoint = {
     "description": """Update an existing song. No content will be returned when the song is updated successfully.""",
     "responses": {
         status.HTTP_204_NO_CONTENT: {
-            "description": "Song updated successfully"},
+            "description": "Song updated successfully"
+            
+            },
+        
+        status.HTTP_400_BAD_REQUEST: {
+            "model": ErrorResponse,
+            "description": "Invalid request body"},
 
         status.HTTP_404_NOT_FOUND: {
             "model": ErrorResponse,
