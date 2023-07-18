956e6ed14a7e53023b6f4b9c79d500d2afeb8cac
jesrolcad <jesrolcad@alum.us.es>
2023-07-18 19:45:18 +0200

    UPDATE GET COUNTRY SERVICE
    Now it raises a NotFoundError
    when the country is not found
    
    Roldán Cadena, Jesús <@jesrolcad>

--- app/logic/services/country_service.py
+++ app/logic/services/country_service.py
@@ -2,12 +2,14 @@ from app.logic.models import Country
 from app.logic.model_mappers.country_model_mapper import CountryModelMapper
 from app.logic.services.base_service import BaseService
 from app.persistence.repositories.country_repository import CountryRepository
-from app.utils.exceptions import AlreadyExistsError
+from app.utils.exceptions import AlreadyExistsError, NotFoundError
 
 
 class CountryService(BaseService):
     def get_country(self, id: int = None, name: str = None, code: str = None)->Country:
         country_entity = CountryRepository(self.session).get_country(id, name, code)
+        if country_entity is None:
+            raise NotFoundError(field="country_id",message=f"Country with id {id} not found")
         return CountryModelMapper.map_to_country_model(country_entity)
 
 
