2ef80287c12f46ad0baedbf5562f66211bf836a8
jesrolcad <jesrolcad@alum.us.es>
2023-08-04 17:40:20 +0200

    FIX UPDATE SONG REPOSITORY METHOD
    Roldán Cadena, Jesús <@jesrolcad>

--- app/persistence/repositories/song_repository.py
+++ app/persistence/repositories/song_repository.py
@@ -46,12 +46,7 @@ class SongRepository(BaseRepository):
                             jury_potential_score=song.jury_potential_score,televote_potential_score=song.televote_potential_score,
                             country_id=song.country_id, event_id=song.event_id))
         
-        result = self.session.execute(update_stmt.returning(SongEntity.id))
-        song_id = result.fetchone()[0]
-
-        song.id = song_id
-        return song
-    
+        self.session.execute(update_stmt)
 
     def delete_song(self, song_id: int):
         delete_stmt = (delete(SongEntity).where(SongEntity.id == song_id))
