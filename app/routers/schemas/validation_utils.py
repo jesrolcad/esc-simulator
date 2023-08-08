363eccd4cc6a3826d8ab2cdbd7907f34be4666aa
jesrolcad <jesrolcad@alum.us.es>
2023-08-08 17:14:27 +0200

    CREATE NOT BLANK FIELD VALIDATOR
    Add method in validation_utils.py file
    
    Roldán Cadena, Jesús <@jesrolcad>

---
+++ app/routers/schemas/validation_utils.py
@@ -0,0 +1,6 @@
+from pydantic_core import PydanticCustomError
+
+def validate_str_not_blank(field: str)->str:
+    if not field.strip():
+            raise PydanticCustomError("blank", "Must not be blank", {"min_length": 1})
+    return field
