9b65968b22960e02030eee4ca70925196b43b6af
jesrolcad <jesrolcad@alum.us.es>
2023-10-25 22:26:07 +0200

    GET EVENT CEREMONIES SERVICE
    Roldán Cadena, Jesús <@jesrolcad>

--- app/logic/services/ceremony_service.py
+++ app/logic/services/ceremony_service.py
@@ -4,6 +4,14 @@ from app.logic.models import Ceremony
 from app.logic.services.base_service import BaseService
 
 class CeremonyService(BaseService):
+
+    def get_event_ceremonies(self, event_id: int)->list[Ceremony]:
+        ceremony_entities = CeremonyRepository(self.session).get_event_ceremonies(event_id=event_id)
+
+        return [CeremonyModelMapper.map_to_ceremony_model_without_event(ceremony_entity=ceremony_entity) 
+                for ceremony_entity in ceremony_entities]
+
+
     def create_ceremony(self, ceremony: Ceremony)->int:
         ceremony_entity = CeremonyModelMapper.map_to_ceremony_entity(ceremony)
         return CeremonyRepository(self.session).create_ceremony(ceremony_entity)
