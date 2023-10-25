6fadf09c58ea148933604df3be6c5d1c11056a11
jesrolcad <jesrolcad@alum.us.es>
2023-10-25 23:40:01 +0200

    INTEGRATION TESTS FOR GET EVENT CEREMONY ENDPOINT
    Roldán Cadena, Jesús <@jesrolcad>

--- app/tests/integration/event/test_cases.py
+++ app/tests/integration/event/test_cases.py
@@ -66,6 +66,65 @@ expected_get_events_response = {
     }
 
 
+expected_get_event_ceremony_response = {
+                "id": 1,
+                "date": "2021-01-01",
+                "ceremony_type": {
+                    "id": 1,
+                    "name": "SEMIFINAL 1",
+                    "code": "SF1"
+                },
+                "songs": [
+                    {
+                        "id": 1,
+                        "title": "SONG1",
+                        "artist": "SONG_ARTIST1",
+                        "belongs_to_host_country": False,
+                        "jury_potential_score": 10,
+                        "televote_potential_score": 10,
+                        "country": {
+                            "id": 1,
+                            "name": "COUNTRY",
+                            "code": "COU"
+                        }
+                    },
+                    {
+                        "id": 2,
+                        "title": "SONG2",
+                        "artist": "SONG_ARTIST2",
+                        "belongs_to_host_country": True,
+                        "jury_potential_score": 10,
+                        "televote_potential_score": 10,
+                        "country": {
+                            "id": 2,
+                            "name": "COUNTRY2",
+                            "code": "CO2"
+                        }
+                    }
+                ],
+                "votings": [
+                    {
+                        "id": 1,
+                        "score": 10,
+                        "voting_country": {
+                            "id": 1,
+                            "name": "COUNTRY"
+                        },
+                        "voted_song": {
+                            "id": 2,
+                            "title": "SONG2",
+                            "country_id": 2,
+                            "country_name": "COUNTRY2"
+                        },
+                        "voting_type": {
+                            "id": 1,
+                            "name": "JURY"
+                        }
+                    }
+                ]
+            }
+
+
 get_events_test_cases = [
     {
         "case": "empty_list",
