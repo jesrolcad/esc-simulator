b3947ef74c1b72565c0bee779b03e127c73af895
jesrolcad <jesrolcad@alum.us.es>
2023-10-25 20:52:41 +0200

    GET EVENT ROUTER UNIT TESTS
    Roldán Cadena, Jesús <@jesrolcad>

--- app/tests/unit/routers/event_endpoints_test.py
+++ app/tests/unit/routers/event_endpoints_test.py
@@ -1,10 +1,13 @@
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 from app.logic.services.event_service import EventService
 from app.logic.models import Event
 from app.main import app
 from app.routers.api_mappers.event_api_mapper import EventApiMapper
 from app.routers.schemas.event_schemas import EventDataResponse
+from app.utils.exceptions import NotFoundError
+
 
 @pytest.fixture
 def client():
@@ -27,5 +30,27 @@ async def test_get_events(mocker, client, event_model, event_schema):
 
     response = client.get("/events")
 
-    assert response.status_code == 200
+    assert response.status_code == status.HTTP_200_OK
     assert response.json() == [event_schema.__dict__]
+
+
+@pytest.mark.asyncio
+async def test_get_event(mocker, client, event_model, event_schema):
+    
+    mocker.patch.object(EventService, "get_event", return_value=event_model)
+    mocker.patch.object(EventApiMapper, "map_to_event_data_response", return_value=event_schema)
+
+    response = client.get("/events/1")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == event_schema.__dict__
+
+
+@pytest.mark.asyncio
+async def test_get_event_not_found(mocker, client):
+
+    mocker.patch.object(EventService, "get_event", side_effect=NotFoundError)
+
+    response = client.get("/events/1")
+
+    assert response.status_code == status.HTTP_404_NOT_FOUND
