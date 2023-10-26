b152cef6575828b1fffdbe21f74e8179cb53a1f5
jesrolcad <jesrolcad@alum.us.es>
2023-10-26 20:11:11 +0200

    CREATE EVENT ENDPOINT
    Roldán Cadena, Jesús <@jesrolcad>

--- app/routers/endpoints/definitions/event_definitions.py
+++ app/routers/endpoints/definitions/event_definitions.py
@@ -1,5 +1,6 @@
 from fastapi import status
 from app.routers.schemas.event_schemas import EventDataResponseList
+from app.routers.schemas.api_schemas import ErrorResponse, ResultResponse
 
 get_events_endpoint = {
     "summary": "Get all events",
@@ -30,6 +31,22 @@ get_event_ceremony_endpoint =  {
         status.HTTP_200_OK: {
             "model": EventDataResponseList,
             "description": "Event ceremonies retrieved successfully"
+        }
+    }
+}
+
+create_event_endpoint = {
+    "summary": "Create event",
+    "description": "Create event. Ceremonies for Semi-Final 1, Semi-Final 2 and Grand Final will be created automatically.",
+    "responses": {
+        status.HTTP_201_CREATED: {
+            "model": ResultResponse,
+            "description": "Event created successfully"
+        },
+
+        status.HTTP_400_BAD_REQUEST: {
+            "model": ErrorResponse,
+            "description": "Invalid request body"
         }
     }
 }
